8d22ae5ea6dbf6c25b337e4c13d1ddea8796b2b4
まーくん <n.marochan-1108@ezweb.ne.jp>
2014-08-31 13:11:10 +0900

    Initial Commit

---
+++ todo list sampleTests/todo_list_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  todo_list_sampleTests.swift
+//  todo list sampleTests
+//
+//  Created by 中村将大 on 2014/08/31.
+//  Copyright (c) 2014年 masa. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class todo_list_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
